103becbaefd76d2a5c1cdf2103910e94d7585a19
lemurwin <merwin.miles@gmail.com>
2018-01-06 17:39:16 -0800

    Initial commit

---
+++ resources/actions.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.cardinfo').hide();
+
+
+  $('.prjimg').hover(function(){
+    $(this).siblings('div').fadeIn();
+  });
+
+  $('.cardinfo').mouseleave(function(){
+    $('.cardinfo').fadeOut();
+    });
+
+  $('#fun').click(function(){
+    $('#meow').get(0).play();
+    $('#funexplode').fadeIn(0, function(){
+      $(this).delay(400).fadeOut('fast');
+      });
+    });
+
+    $('.hoverimg').hover(function(){
+      $(this).attr('src', 'resources/1.png');
+    });
+    $('.hoverimg').mouseleave(function(){
+      $(this).attr('src', 'resources/2.png')
+    });
+  });
